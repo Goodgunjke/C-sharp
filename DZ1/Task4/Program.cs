ded2815f9cf51452ee664d7f7708422146ac382d
Alex Var <KvE1N@ya.ru>
2023-01-19 22:34:51 +0300

    Added condition to task 4

--- DZ1/Task4/Program.cs
+++ DZ1/Task4/Program.cs
@@ -1,4 +1,5 @@
-﻿int max = int.MinValue; 
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. 2, 3, 7 -> 7 ; 44 5 78 -> 78 ; 22 3 9 -> 22
+int max = int.MinValue; 
 for (int i = 0; i < 3; i++) 
 {
    int num = Convert.ToInt32(Console.ReadLine()); 
